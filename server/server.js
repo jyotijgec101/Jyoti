09609f6ad4ae94e6def560815211369036d0a535
Souhardya Deb <104934508+DarkWarrior2003@users.noreply.github.com>
2024-02-16 22:47:54 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const contactrouter = require("./router/contact-router")
 const adminrouter = require("./router/admin-router");
 const connectDb = require("./utils/db");
 const corsoption = {
-    origin : "https://jyoti-mern-ghvc.vercel.app",
+    origin : "https://jyoti-jgec-new.vercel.app",
     methods : "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials : true, 
 }
