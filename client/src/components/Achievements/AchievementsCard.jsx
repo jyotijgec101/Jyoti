a524562fb3e423b1016d240abcbb54010513c595
Souhardya Deb <104934508+DarkWarrior2003@users.noreply.github.com>
2024-02-20 01:20:34 +0530

    Add files via upload

--- client/src/components/Achievements/AchievementsCard.jsx
+++ client/src/components/Achievements/AchievementsCard.jsx
@@ -5,23 +5,13 @@ const AchievementsCard = (props) => {
     <div className="main hover:scale-110 transition-transform ">
   <ul className="cards  ">
     <li className="cards_item ">
-      <div className="card ">
+      <div className="card hover:text-white hover:bg-orange-400">
         <div className="card_image border-[3px]  border-amber-700 h-48">
           <img src={props.img} alt="mixed vegetable salad in a mason jar." />
          
         </div>
-        <div className="card_content h-28">
+        <div className="card_content  hover:text-white hover:bg-orange-400 h-28">
           <h2 className="card_title">{props.title}</h2>
-          {/* <div className="card_text">
-            <p>Dig into the freshest veggies of the season! This salad-in-a-jar features a mixture of
-              leafy greens and seasonal vegetables, fresh from the farmer's market.
-            </p>
-            <hr />
-            <p>Served with your choice of dressing on the side: <strong>housemade ranch</strong>, <strong>cherry balsamic
-                vinaigrette</strong>, <strong>creamy chipotle</strong>, <strong>avocado green goddess</strong>, or <strong>honey mustard</strong>. Add your choice
-              of protein for $2 more.
-            </p>
-          </div> */}
         </div>
       </div>
     </li>
--- client/src/components/Notice/Notice.jsx
+++ client/src/components/Notice/Notice.jsx
@@ -6,13 +6,13 @@ const Notice = () => {
 
   return (
     <div className=" flex-col noticeback  h-full">
-      <div className="flex justify-center pt-8 items-center flex-col">
+      <div className="flex justify-center pt-8 fadein items-center flex-col">
         <h3 className="text-center font-semibold text-4xl text-amber-700 pt-5 ">
           NOTICE BOARD
         </h3>
-        <div className="border-[2px]  border-amber-600 mt-2 w-[10%]"></div>
+        <div className="border-[2px] border-amber-600 mt-2 w-[10%]"></div>
       </div>
-      <div className="row">
+      <div className="row ">
         <div className="col-md-12">
           <div id="content" className="content  content-full-width">
             <div className="profile-content py-8">
@@ -20,9 +20,9 @@ const Notice = () => {
                 <div className="tab-pane fade active show" id="profile-post">
                   <ul className="timeline ">
                     {service &&
-                      service.map((item, e) => (
+                      service.slice(0).reverse().map((item, e) => (
                         <li key={e}>
-                          <div className="timeline-time lg:block md:block hidden">
+                          <div className="timeline-time slideleft lg:block md:block hidden">
                             <span className="date">{item.date}</span>
                           </div>
 
@@ -30,11 +30,11 @@ const Notice = () => {
                             <a href="/">&nbsp;</a>
                           </div>
 
-                          <div className="timeline-body ">
+                          <div className="timeline-body slideright hover:scale-[1.03] transition-[1] ">
                             <div className="timeline-header">
                               <span className="username">
-                                <p className=" text-right text-sm font-medium">
-                                  Notice {e + 1}
+                                <p className=" text-right text-blue-500 italic text-[13px] font-medium">
+                                  Notice {service.length - e}
                                 </p>
                               </span>
                             </div>
@@ -43,7 +43,7 @@ const Notice = () => {
                                 href={item.url}
                                 target="_blank"
                                 rel="noreferrer"
-                                className="text-[18px] text-black font-medium"
+                                className="text-[18px] font-sans text-black font-medium"
                               >
                                 {item.title}
                               </a>
